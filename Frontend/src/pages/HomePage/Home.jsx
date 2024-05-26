44c05f810b7892e943078dfa2bc5342f7dc59ef7
naya-web <nayaalnakola@gmail.com>
2024-05-26 18:11:15 +0300

    header not completed in front end

--- Frontend/src/pages/HomePage/Home.jsx
+++ Frontend/src/pages/HomePage/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Header from '../../components/AllComponents/Header/Header'
 // import NavBar from '../../components/AllComponents/NavBar/NavBar'
 // import Footer from '../../components/AllComponents/Footer/Footer'
 
 const Home = () => {
   return (
     <>
-      Homepage
+      <Header/>
     </>
   )
 }
