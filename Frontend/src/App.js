e118b67548992ec7293b274d1cbff848d7b96414
naya-web <nayaalnakola@gmail.com>
2024-05-26 14:47:56 +0300

    change routers

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,22 +1,17 @@
-import './App.css';
-import { Routes, Route } from "react-router-dom";
-import Home from './pages/HomePage/Home';
-import Menu from './pages/Menu/Menu';
-import Basket from './pages/Basket/Basket'
-import Login from './pages/Login/Login';
-import SignUp from './pages/SignUp/SignUp';
-
+import 'bootstrap/dist/css/bootstrap.css';
+import RootLayout from "./RootLayout";
 
 function App() {
   return (
    <>
-   <Routes>
+   {/* <Routes>
     <Route path="/" element={<Home/>}></Route>
     <Route path="/menu" element={<Menu/>}></Route>
     <Route path='/basket' element={<Basket/>}></Route>
     <Route path='/login' element={<Login/>}></Route>
     <Route path='./signup' element={<SignUp/>}></Route>
-   </Routes>
+   </Routes> */}
+   <RootLayout/>
    </>
   );
 }
--- Frontend/src/pages/HomePage/Home.jsx
+++ Frontend/src/pages/HomePage/Home.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import NavBar from '../../components/AllComponents/NavBar/NavBar'
-import Footer from '../../components/AllComponents/Footer/Footer'
-
+// import NavBar from '../../components/AllComponents/NavBar/NavBar'
+// import Footer from '../../components/AllComponents/Footer/Footer'
 
 const Home = () => {
   return (
-    <div>
-      Naya
-      <NavBar/>
-      <Footer/>
-    </div>
+    <>
+      Homepage
+    </>
   )
 }
 
