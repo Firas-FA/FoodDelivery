b21146437d8464390228a76c17fde36cc48fc0e3
Noura-Bdr-Aldeen <nourabdraldeen@gmail.com>
2024-05-27 14:53:08 +0300

    new

--- Frontend/src/components/AllComponents/NavBar/NavBar.jsx
+++ Frontend/src/components/AllComponents/NavBar/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
   
 window.addEventListener('DOMContentLoaded', event => {
 
-  // Navbar shrink function
+
   var navbarShrink = function () {
       const navbarCollapsible = document.body.querySelector('#mainNav');
       if (!navbarCollapsible) {
