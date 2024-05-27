2e4d21c493ca7314091914680846227d12e28cb3
Firas-FA <firascanva505@gmail.com>
2024-05-27 05:52:50 -0700

    complet singup with backend

--- Frontend/src/pages/SignUp/SignUp.jsx
+++ Frontend/src/pages/SignUp/SignUp.jsx
@@ -1,59 +1,147 @@
-import { NavLink } from 'react-router-dom'
-
-
+import React, { useState } from "react";
+import axios from "axios";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+  const [name, setUserName] = useState("");
+  const [email, setUserEmail] = useState("");
+  const [password, setUserPassword] = useState("");
+  const [responseMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+
+  async function handleSignUp(e) {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:5000/register", {
+        userName: name,
+        userEmail: email,
+        userPassword: password,
+      });
+
+      setUserName("");
+      setUserEmail("");
+      setUserPassword("");
+      setErrorMessage(response.data.message);
+      navigate("/login");
+    } catch (error) {
+      error.response.data.details ===
+      "User validation failed: userPassword: Password must be at least 6 characters long."
+        ? setErrorMessage("Password must be at least 6 characters long")
+        : setErrorMessage(error.response.data.details);
+    }
+  }
+
   return (
     <>
-      <div class='Container p-5'>
-        <div class='row mx-0 pt-5 pb-5'>
-          <div class=" col-lg-6 pt-5">
-            <img src="https://img.freepik.com/free-photo/gourmet-italian-bolognese-pasta-with-fresh-parmesan-generated-by-ai_188544-9453.jpg?w=740&t=st=1716721493~exp=1716722093~hmac=f5080dd74c457aff9c7920ea0af1e74e49e5f8b1864109286c19445b1f5456ab" alt="" />
+      <div className="Container p-5">
+        <div className="row mx-0 pt-5 pb-5">
+          <div className="col-lg-6 pt-5">
+            <img alt="" />
           </div>
-          <div class="col-lg-6 pt-4">
+          <div className="col-lg-6 pt-4">
             <div className="container-form">
-              <form class="form">
-                <p id="heading">Log<span style={{ color: "#FDCE77" }}>in</span></p>
-
-                <div class="field">
-                  <svg class="input-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+              <form className="form" onSubmit={handleSignUp}>
+                <p id="heading">
+                  Sign<span style={{ color: "#FDCE77" }}>Up</span>
+                </p>
+                <div className="field">
+                  <svg
+                    className="input-icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
+                  >
                     <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z"></path>
                   </svg>
-                  <input  autocomplete="off" placeholder="UserName" class="input-field" type="text"
-                
+                  <input
+                    value={name}
+                    onChange={(e) => {
+                      setUserName(e.target.value);
+                    }}
+                    autoComplete="off"
+                    placeholder="UserName"
+                    className="input-field"
+                    type="text"
                   />
                 </div>
-                <div class="field">
-                  <svg class="input-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <div className="field">
+                  <svg
+                    className="input-icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
+                  >
                     <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z"></path>
                   </svg>
-                  <input autocomplete="off" placeholder="userEmail" class="input-field" type="email"
-                    
+                  <input
+                    value={email}
+                    onChange={(e) => {
+                      setUserEmail(e.target.value);
+                    }}
+                    autoComplete="off"
+                    placeholder="userEmail"
+                    className="input-field"
+                    type="text"
                   />
                 </div>
-                <div class="field">
-                  <svg class="input-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <div className="field">
+                  <svg
+                    className="input-icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
+                  >
                     <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"></path>
                   </svg>
-                  <input placeholder="Password" class="input-field" type="password"
-                    
+                  <input
+                    value={password}
+                    onChange={(e) => {
+                      setUserPassword(e.target.value);
+                    }}
+                    placeholder="Password"
+                    className="input-field"
+                    type="password"
                   />
                 </div>
-                <div class="btn">
-                  <button class="button1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Log<span style={{ color: "#FDCE77" }}>in</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
-                  <NavLink to={'/login'} >
-                    <button  class="button2">Sign <span style={{ color: "#FDCE77" }}>Up</span></button>
+                {responseMessage && (
+                  <p
+                    style={{
+                      color:
+                        responseMessage === "🎉 Account created successfully!"
+                          ? "#25d366"
+                          : "#fe0000",
+                    }}
+                  >
+                    {responseMessage}
+                  </p>
+                )}
+                <div className="btn">
+                  <NavLink to={"/login"}>
+                    <button className="button1">
+                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Log
+                      <span style={{ color: "#FDCE77" }}>in</span>
+                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    </button>
                   </NavLink>
-
+                  <button type="submit" className="button2">
+                    Sign <span style={{ color: "#FDCE77" }}>Up</span>
+                  </button>
                 </div>
-                <button class="button3">Forgot Password</button>
+                <button className="button3">Forgot Password</button>
               </form>
             </div>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
